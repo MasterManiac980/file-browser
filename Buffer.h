2a59bdda931a4d645b5edb46a6e5ee2ca72ab3a7
MasterManiac980 <trainboy980@gmail.com>
2022-10-04 19:42:46 -0400

    finished outline of Buffer

--- Buffer.h
+++ Buffer.h
@@ -7,8 +7,15 @@
 class Buffer
 {
 public:
+    void readFile(const char fileName[]);
     void display();
+    void setViewableLines(int numOfLines);
+    void openLink(int linkNumber);
 
 private:
     std::vector<std::string> m_bufferData;
+    std::vector<std::string> m_fileNames;
+    std::vector<int> m_history;
+    int m_topLineNum;
+    int m_viewableLines;
 };
