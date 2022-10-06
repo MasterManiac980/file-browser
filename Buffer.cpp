2af8bffba4bb7c8c618df7f73e41e75016556ac4
MasterManiac980 <trainboy980@gmail.com>
2022-10-06 14:06:16 -0400

    starting restructure of Buffer::readFile() and Buffer::display()

--- Buffer.cpp
+++ Buffer.cpp
@@ -6,26 +6,24 @@
 
 void Buffer::display()
 {
-    for (size_t i = m_topLineNum; i < (m_topLineNum + m_viewableLines); i++)
-    {
-        std::cout << std::right << std::setw(3) << i + 1 << std::left << std::setw(3) << " " << m_bufferData[i] << std::endl;
-    }
 }
 
 void Buffer::nextPage()
 {
     m_topLineNum += m_viewableLines;
-    display();
 }
 
 void Buffer::openLink(int linkNumber)
 {
+    linkNumber -= 1; // Converts number provided to index for accessing file name in vector
     m_bufferData.clear();
     readFile(m_fileNames[linkNumber]);
 }
 
 void Buffer::readFile(std::string fileName)
-/* TODO: Modify the input based on the tags provided in the text
+/*
+
+    TODO: Modify the input based on the tags provided in the text
 
     1. Look for < in each line
     2. After each <, take use >> to take in the tag type
@@ -37,26 +35,13 @@ void Buffer::readFile(std::string fileName)
 */
 {
     std::ifstream infile(fileName);
-    std::string currentLine;
-    if (infile.is_open())
+    if (!infile.fail())
     {
-        while (!infile.eof())
-        {
-            infile >> currentLine;
-            if (infile.eof()) break;
-            if (currentLine == "<a") {
-                std::string fileDescriptor = "<";
-                infile >> fileDescriptor;
-                infile >> m_fileNames.push_back();
-            }
-            else if {
-
-            }
-            else if {
-
-            }
-            m_bufferData.push_back(currentLine);
-        }
+    }
+    else
+    {
+        
+        return;
     }
     infile.close();
 }
