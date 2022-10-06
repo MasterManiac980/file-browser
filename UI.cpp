bf902956da161736969e54514870be5b25f398f8
Tyler Cady <cadytm@clarkson.edu>
2022-10-06 13:30:26 -0400

    outline for UI

--- UI.cpp
+++ UI.cpp
@@ -1,4 +1,35 @@
 #include <iostream>
-
 #include "Buffer.h"
 #include "UI.H"
+
+using namespace std; 
+
+void Buffer::display() const 
+{
+    
+} 
+void execute(char selection, bool & done)
+{
+switch (selection) {
+     
+        case 'n': {
+            //Next page    
+            break;
+        }
+
+        case 'o': {
+            //open file 
+            break;
+        }
+
+        case 'p': {
+            //previous page
+            break;
+        }
+
+        case 'q': {
+            //quit
+            break;
+        }
+    }
+}
