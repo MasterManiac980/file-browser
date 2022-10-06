c55a3cb214acc4f529ea9136b0acca1ee08025ef
Tyler Cady <cadytm@clarkson.edu>
2022-10-06 14:19:01 -0400

    display is gone but not from our hearts

--- UI.cpp
+++ UI.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
+#include <iomanip>
 #include "Buffer.h"
 #include "UI.H"
 
 using namespace std; 
+const string Short_Ast(10,'*');
+const string Long_Ast(80,'*'); 
 
-void Buffer::display() const 
-{
-    
-} 
 void execute(char selection, bool & done)
 {
 switch (selection) {
