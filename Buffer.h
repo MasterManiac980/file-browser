9eacffaf582fa28ff65146446675510966b1c54b
MasterManiac980 <trainboy980@gmail.com>
2022-10-11 17:55:48 -0400

    reorganized data declarations in Buffer.h and added to design.txt

--- Buffer.h
+++ Buffer.h
@@ -20,17 +20,19 @@ public:
     void openFile(std::string fileName);
 
     // Error Reporting
-    void printError(std::ostream& out = std::cout);
+    void printError(std::ostream &out = std::cout);
     void setUIError(std::string errorMessage);
 
 private:
     std::vector<std::string> m_bufferData;
-    std::vector<std::string> m_fileNames;
-    std::vector<std::string> m_history;
     int m_topLineNum;
     int m_viewableLines;
     int m_charsPerLine;
+
+    std::vector<std::string> m_fileNames;
+    std::vector<std::string> m_history;
     std::string m_currentFileName;
+
     std::string m_BufferErrorMessage;
     std::string m_UIErrorMessage;
 };
