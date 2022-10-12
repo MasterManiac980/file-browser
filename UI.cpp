853ff2a4e5b6f58dbe28c466b010961ea0ab869a
Tyler Cady <cadytm@clarkson.edu>
2022-10-11 23:12:06 -0400

    UI Jazz

--- UI.cpp
+++ UI.cpp
@@ -6,29 +6,27 @@
 #include "UI.H"
 
 using namespace std; 
-const string Short_Ast(10,'*');
-const string Long_Ast(80,'*'); 
+ 
 
-void UI::run()
+void UI::display()
 {
-    cout << "Enter Window Height: ";
-    cin >> m_viewableLines;
-    cin.get();  // '\n'
-    cout << '\n';
-    m_buffer.setViewableArea(m_viewableLines);
+    const string Short_Dash(10,'-');
+    const string Long_Dash(80,'-');
+    //Clear screen isn't working. Ask Cary. 
 
-    bool done = false;
-    while (!done) {
-        display();
-        cout << "command: ";
-        char command;
-        cin >> command;
-        cin.get(); 
-        execute(command, done);
-
-        cout << endl;
+    //If there is an error message 
+    if (!error_message_.empty()) {
+        cout << "ERROR: " + error_message_ << endl;
+        error_message_.clear();
     }
-void UI::execute(char selection, bool & done)
+    string file_name = m_buffer.;
+
+
+
+
+}
+
+void UI::execute(char selection, bool & isDone)
 {
 switch (selection) {
      
@@ -43,18 +41,61 @@ switch (selection) {
             cout << "file name: ";
             string file_name;
             getline(cin, file_name);
-            m_buffer.openFile(file_name);
+
+            if(file_name.substr(0,1) != "<")
+            {
+                m_buffer.openFile(file_name);
+            }
+            else
+            {
+                int file_num;
+                file_num = stoi(file_name);
+                m_buffer.openLink(file_num);
+                
+                if (!m_buffer.openLink(file_num))
+                {
+                error_message_ = "Could not open " + file_name;
+                }
+            }
+
             break;
         }
 
         case 'p': {
             //previous page
+            m_buffer.openLastFile();
             break;
         }
 
         case 'q': {
             //quit
+            isDone = true;
             break;
         }
+    }
+}
+
+void UI::run()
+{
+    cout << "Enter Window Height: ";
+    cin >> m_vertical_lines;
+    cin.get();  // '\n'
+    cout << '\n';
+    cout <<"Enter Window Width: ";
+    cin >> m_horizontal_lines;
+    cin.get();  // '\n'
+    cout << '\n';
+    m_buffer.setViewableArea(m_vertical_lines,m_horizontal_lines);
+
+    bool isDone = false;
+    while (!isDone) {
+        display();
+        cout << "command: ";
+        char command;
+        cin >> command;
+        cin.get(); 
+        execute(command, isDone);
+
+        cout << endl;
     }
 }
